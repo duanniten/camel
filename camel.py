18ad0affb1d645fe35c0f84b242f9dfe6e1d824c
duanniten <duann_@hotmail.com>
2024-09-19 00:10:06 -0300

    implement fromCameToSnake

---
+++ camel.py
@@ -0,0 +1,13 @@
+def main():
+    var_came = input("camelCase: ")
+    var_snake = fromCameToSnake(var_came)
+    print("snake_case:", var_snake)
+
+def fromCameToSnake(var_name):
+    for lether in var_name:
+            camel = ""
+            if lether.isupper():
+                camel = camel + "_" + lether.lower()
+            else:
+                camel = camel + lether
+    return camel
